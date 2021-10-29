9800be51518193cd90842ba93c8afb8d57fd63fa
Barkarov <michaelbarkarov@gmail.com>
2021-10-29 15:44:43 +0300

    dialogs changes

--- my-app/src/componets/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/componets/Profile/MyPosts/MyPosts.jsx
@@ -7,7 +7,7 @@ const MyPosts = (props) => {
  
 
     let postsElements =
-      props.posts.map ( p => <Post message={p.message} likesCount={p.likesCount}/>);
+      props.posts.map ( p => <Post message={p.message} likesCount={p.likesCount}/>); 
 
 
     return (
